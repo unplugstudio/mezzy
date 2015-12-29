59571e3ff77a2407cfaa3359d4d5f63e150c960c
Eduardo Rivas <e@jerivas.com>
2015-12-28 23:04:09 -0600

    Make it pip-installable.

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+from mezzy import __version__
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mezzy',
+    version=__version__,
+    description='Useful utilities for Mezzanine projects',
+    long_description=long_description,
+    url='https://gitlab.com/tigris-webdev/mezzy',
+    author='Ed Rivas',
+    author_email='e@jerivas.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+    ],
+    keywords='django mezzanine utility helper',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True
+)

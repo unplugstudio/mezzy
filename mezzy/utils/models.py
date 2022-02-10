9795455f5a42c1c82e5371bd33edb555ba95899f
Tito Merino <tito.merino@unplug.studio>
2022-02-10 09:48:28 -0600

    feat: remove compatibility with python 2.7

--- mezzy/utils/models.py
+++ mezzy/utils/models.py
@@ -1,12 +1,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 from mezzanine.core.models import Slugged, Orderable
 
 
-@python_2_unicode_compatible
 class Titled(models.Model):
     """
     Inline model with a title.
